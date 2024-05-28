1cb4c275b9c100c2097209aa046c7a061845bbc5
TheAFKGamer10 <107822814+TheAFKGamer10@users.noreply.github.com>
2024-05-28 16:56:37 -0500

    Update snailycadptero.js

--- snailycadptero.js
+++ snailycadptero.js
@@ -24,7 +24,6 @@ fetch('https://www.random.org/strings/?num=1&len=32&digits=on&upperalpha=on&lowe
         execSync(`apt install postgresql postgresql-contrib -y`)
         execSync(`npm install -g pnpm`);
         execSync(`service postgresql start`)
-        execSync(`systemctl enable postgresql.service`)
 
         execSync(`psql -h localhost -d postgres -U postgres -c "CREATE USER snailycad WITH PASSWORD '${data['POSTGRES_PASSWORD']}' SUPERUSER"`)
         execSync(`psql -h localhost -d postgres -U postgres -c "SELECT 'CREATE DATABASE snaily-cadv4 WITH OWNER = ${data['POSTGRES_USER']}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'snaily-cadv4')\gexec"`)
