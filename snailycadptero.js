e4c0bce70fba33a1662a17a1f18ee7c249dc0c07
TheAFKGamer10 <107822814+TheAFKGamer10@users.noreply.github.com>
2024-05-29 16:53:11 -0500

    Update snailycadptero.js

--- snailycadptero.js
+++ snailycadptero.js
@@ -57,10 +57,7 @@ fetch('https://www.random.org/strings/?num=1&len=32&digits=on&upperalpha=on&lowe
 
         execSync(`node scripts/copy-env.mjs --client --api`, { stdio: 'inherit' });
         execSync(`echo "Building"`, { stdio: 'inherit' });
-        execSync(`pnpm run build`, { stdio: 'inherit' });
-        
-        execSync(`npm install npm`, { stdio: 'inherit' });
-        execSync(`npm install pnpm`, { stdio: 'inherit' });
+        execSync(`pnpm run build`, { stdio: 'inherit' })
 })
     .catch(error => {
         console.error('Error:', error);
